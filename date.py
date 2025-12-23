f8fef4434f1eff653b09477b66804ecb9f3e1467
2023095900028472bscit-sudo <2023095900028472_bscit@spcevng.ac.in>
2025-12-23 10:38:56 +0530

    Update date.py

--- date.py
+++ date.py
@@ -1,3 +1,11 @@
-import datetime
-current_datetime = datetime.datetime.now()
-print(current_datetime)
+from datetime import datetime
+
+
+now = datetime.now()
+
+
+formatted_datetime = now.strftime("%b %d, %Y %I:%M:%S %p")
+
+
+print(formatted_datetime)
+
